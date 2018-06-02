e4b77550ea21941640302691df4b765a33b3a2ba
Erik Lydersen <erikly@gmail.com>
2018-06-02 15:20:11 +0200

    Cleanup

---
+++ BeerService/Controllers/BeerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeerService.Controllers
+{
+    [Route("api/[controller]")]
+    public class BeerController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+        
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+    }
+}
